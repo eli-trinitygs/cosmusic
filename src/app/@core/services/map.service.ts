2722a2724fe90788b5f1791da65f4bc113e678ef
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-31 09:31:55 -0600

    creando servicio para el mapa

---
+++ src/app/@core/services/map.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+//import MapBox
+import * as MapBoxgl from 'mapbox-gl';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+
+  constructor() { }
+}
