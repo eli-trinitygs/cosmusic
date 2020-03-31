20408434247702f6c3e3d1d0444d699af393dc97
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-31 09:16:31 -0600

    agregando mapbox

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatMenuModule} from '@angular/material/menu';
-
+import {MatCardModule} from '@angular/material/card';
 
 
 const MaterialComponents = [
   MatButtonModule,
   MatSidenavModule,
-  MatMenuModule
+  MatMenuModule,
+  MatCardModule
 ]
 
 @NgModule({
