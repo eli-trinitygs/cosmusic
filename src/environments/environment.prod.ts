42e4c62dbc782020ed0d8a48996356761cfb8f40
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-31 09:21:07 -0600

    agregando token en enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoiZWxpemFiZXRodHJpbml0eWlrciIsImEiOiJjazhmbmZkNjUwNjVwM2xueno2YWk5eWdnIn0.XgZxBvXhKqVcLWvXL71OfA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapBoxToken: 'pk.eyJ1IjoiZWxpemFiZXRodHJpbml0eWlrciIsImEiOiJjazhmbmZkNjUwNjVwM2xueno2YWk5eWdnIn0.XgZxBvXhKqVcLWvXL71OfA'
 };
 
 /*
