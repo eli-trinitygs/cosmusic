c7d08d3562d4268601ea4b9a51f2e6ad8b0103fa
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-31 09:40:16 -0600

    creando la visualicion del mapa

--- src/app/@core/services/map.service.ts
+++ src/app/@core/services/map.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
 //import MapBox
-import * as MapBoxgl from 'mapbox-gl';
+import * as Mapboxgl from 'mapbox-gl';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MapService {
+export class MapService implements OnInit {
+
+map: Mapboxgl.Map;
+  ngOnInit() {
+    //llamando enviroments
+    (Mapboxgl as any).accessToken = environment.mapBoxToken;
+this.map = new Mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-99.1433941, 19.435205,], // starting position
+zoom: 17 // starting zoom
+});
+    
+  }
 
   constructor() { }
 }
